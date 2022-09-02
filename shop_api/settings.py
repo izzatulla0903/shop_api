a50e0baff4dd6c0cff2c9318f13ff9ab4bc3ab26
izzatulla0903 <izzatulla.biibek@yandex.ru>
2022-09-02 20:16:59 +0600

    change

--- shop_api/settings.py
+++ shop_api/settings.py
@@ -142,8 +142,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+STATIC_URL = '/staticfiles1/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles1/')
 
 MEDIA_URL = 'media/'
 MEDIA_ROOT = BASE_DIR / 'media'
